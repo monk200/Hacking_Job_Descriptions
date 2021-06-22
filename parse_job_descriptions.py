d370add50fca49d25bf795675ea3c431b0cedbaf
Monika <80641920+monk200@users.noreply.github.com>
2021-06-22 16:08:44 -0500

    Count bigrams disregarding their order

--- parse_job_descriptions.py
+++ parse_job_descriptions.py
@@ -19,7 +19,17 @@ def cleanData(jobDesc):
                   "new", "skills", "engineer", "computer", "us", "required", "ability", "support", "develop", "development",
                   "must", "engineers", "knowledge", "degree", "years", "world", "including", "related", "position",
                   "employer", "products", "status", "responsibilities", "role", "opportunity", "employees", "company",
-                  "may", "equal", "disability", "looking", "make", "employment", "national", "qualifications"]
+                  "may", "equal", "disability", "looking", "make", "employment", "national", "qualifications", "customers",
+                  "general", "help", "life", "bachelor", "etc", "benefits", "applicants", "gender", "customer", "level",
+                  "using", "industry", "protected", "part", "provide", "preferred", "tasks", "apply", "join", "andor",
+                  "identity", "individuals", "one", "qualified", "travel", "product", "global", "insurance", "include",
+                  "future", "race", "religion", "sexual", "orientation", "people", "quality", "meet", "government",
+                  "without", "opportunities", "best", "medical", "age", "use", "please", "duties", "veteran", "every",
+                  "specific", "implement", "perform", "next", "responsible", "procedures", "across", "employee",
+                  "able", "capabilities", "bring", "candidates", "regard", "based", "closely", "sex", "diverse", "professional",
+                  "like", "specifications", "familiarity", "day", "candidate", "excellent", "area", "learn", "also",
+                  "processes", "clearance", "diversity", "want", "access", "background", "standard", "minimum", "need",
+                  "aurora"]
     for word in irrelevant:
         stopWords.append(word)
     filteredWords = nltk.FreqDist(word.lower() for word in words if word.lower() not in stopWords)
@@ -30,11 +40,32 @@ def cleanData(jobDesc):
 # TODO: bigrams and trigrams only ever appear once because every post is written uniquely, try to find a way to rank freq regardless of word order
 def bigrams_and_trigrams(filteredWords):
     # individual words
-    solograms = filteredWords.most_common(30)
+    solograms = filteredWords.most_common(100)
 
     # bigrams
-    findBigrams = nltk.bigrams(filteredWords)
-    bigrams = (nltk.FreqDist(findBigrams)).most_common(30)
+    # these two lines will return the 30 most common bigrams, preserving their order
+    #findBigrams = nltk.bigrams(filteredWords)
+    #bigrams = (nltk.FreqDist(findBigrams)).most_common(30)
+
+    # these lines will return a sorted list of the most common bigrams, disregarding their order
+    findBigrams = list(nltk.bigrams(filteredWords))
+    foundList = []
+    bigrams = []
+    # loop through list of discovered bigrams
+    for i in range(0, len(findBigrams)):
+        # no need to look for duplicates if we've already found this combination
+        if findBigrams[i] in foundList:
+            continue;
+        count = 1
+
+        for j in range(i+1, len(findBigrams)):      # look through and compare to other bigrams
+            # find when same tokens or when swapped tokens
+            if ((findBigrams[i][0] == findBigrams[j][0]) and (findBigrams[i][1] == findBigrams[j][1]))\
+                    or ((findBigrams[i][0] == findBigrams[j][1]) and (findBigrams[i][1] == findBigrams[j][0])):
+                count += 1
+                foundList.append(findBigrams[j])       # add to foundList because we don't want to have duplicates in final list
+
+        bigrams.append((findBigrams[i], count))
 
     # trigrams
     findTrigrams = nltk.trigrams(filteredWords)
